41f98f1434b803b2a742d2e9630927cde9b3408c
Will Kennedy <theawesomew@gmail.com>
2026-07-26 21:45:54 +1000

    feat(add-ReLU-activation): Added ReLU activation after the first layer and updated backpropagation to reflect that. Added epoch in training with training data shuffling. Added learning rate decay per-epoch

--- src/mnist.nim
+++ src/mnist.nim
@@ -1,3 +1,16 @@
+# z_1 = ReLU(W_1 * x + b_1)
+# z_2 = W_2 * z_1 + b_2
+# o = softmax(z_2)
+# L(o, y) = categoricalCrossEntropyLoss(o, y)
+#
+# dL/dz_2 = o - y
+#
+# dL/dW_2 = dL/dz_2 * dz_2/dW_2 = (o - y) * ReLU(W_1 * x + b_1) -> (1, 10) * (1, 24)^T
+# dL/dW_1 = dL/dz_2 * dz_2/dz_1 * dz_1/dW_1 = (o - y) * W_2 * ReLU'(W_1 * x + b_1) * x -> (784, 1) * (1, 10) * (24, 10)^T @ (1, 24)
+# dL/db_2 = dL/dz_2 * dz_2/db_2 = (o - y) * 1
+# dL/db_1 = dL/dz_2 * dz_2/dz_1 * dz_1/db_1 = (o - y) * W_2 * ReLu'(W_1 * x + b_1) * 1
+
+
 import std/[strutils, sequtils, math, random, assertions]
 
 proc clamp(x, lo, hi: float64): float64 =
@@ -24,42 +37,55 @@ proc softmax(xs: seq[seq[float64]]): seq[seq[float64]] =
   for i in 0 ..< result.len:
     result[i][0] /= s
 
-proc matmul (a, b: seq[seq[float64]]): seq[seq[float64]] =
-  result = @[]
+proc ReLU (xs: seq[seq[float64]]): seq[seq[float64]] = xs.mapIt(it.mapIt(max(0, it)))
 
+proc dReLU (xs: seq[seq[float64]]): seq[seq[float64]] =
+  result = newSeqWith(xs.len, newSeq[float64](xs[0].len))
+
+  for i, x in xs:
+    for j, a in x:
+      if a <= 0:
+        result[i][j] = 0
+      else:
+        result[i][j] = 1
+
+proc matmul (a, b: seq[seq[float64]]): seq[seq[float64]] =
   if len(b) != len(a[0]):
     echo $len(b[0]) & " x " & $len(b)
     echo $len(a[0]) & " x " & $len(a)
     raise newException(ValueError, "Matrix dimensionality is wrong")
 
+  result = newSeqWith(len(a), newSeq[float64](b[0].len))
+
   for j in 0..<len(a):
-    var r = newSeq[float64]()
     for i in 0..<len(b[0]):
       var sum: float64 = 0.0
       for k in 0..<len(a[0]):
         sum += b[k][i] * a[j][k]
-      r.add(sum)
-    result.add(r)
+      result[j][i] = sum
 
-proc `*` (a: float64, b: seq[seq[float64]]): seq[seq[float64]] = b.map(proc (x: seq[float64]): seq[float64] = x.map(proc (y: float64): float64 = y * a))
+proc `*` (a: float64, b: seq[seq[float64]]): seq[seq[float64]] = b.mapIt(it.mapIt(it * a))
+
+proc `*` (a, b: seq[seq[float64]]): seq[seq[float64]] =
+  result = newSeqWith(a.len, newSeq[float64](a[0].len))
+
+  for i in 0..<len(a):
+    for j in 0..<a[0].len:
+      result[i][j] = a[i][j] * b[i][j]
 
 proc `+`(a, b: seq[seq[float64]]): seq[seq[float64]] =
-  result = @[]
+  result = newSeqWith(len(a), newSeq[float64](a[0].len))
 
   for i in 0..<len(a):
-    var r = newSeq[float64]()
     for j in 0..<len(a[0]):
-      r.add(a[i][j] + b[i][j])
-    result.add(r)
+      result[i][j] = a[i][j] + b[i][j]
 
 proc `-`(a, b: seq[seq[float64]]): seq[seq[float64]] =
-  result = @[]
+  result = newSeqWith(len(a), newSeq[float64](a[0].len))
 
   for i in 0..<len(a):
-    var r = newSeq[float64]()
     for j in 0..<len(a[0]):
-      r.add(a[i][j] - b[i][j])
-    result.add(r)
+      result[i][j] = a[i][j] - b[i][j]
 
 proc transpose (a: seq[seq[float64]]): seq[seq[float64]] =
   result = @[]
@@ -81,15 +107,15 @@ type NN = object
   B2: seq[seq[float64]]
 
 proc forward (nn: NN, x: seq[seq[float64]]): seq[seq[float64]] =
-  let z1 = matmul(nn.W1, x) + nn.B1
+  let z1 = ReLU(matmul(nn.W1, x) + nn.B1)
   let z2 = matmul(nn.W2, z1) + nn.B2
   return softmax(z2)
 
 proc backward (nn: var NN, o: seq[seq[float64]], y: seq[seq[float64]], x: seq[seq[float64]], alpha: float64): void =
-  let dW2 = matmul((o - y), (matmul(nn.W1, x) + nn.B1).transpose())
-  let dW1 = matmul(matmul(nn.W2.transpose(), (o - y)), x.transpose())
+  let dW2 = matmul((o - y), ReLU(matmul(nn.W1, x) + nn.B1).transpose())
+  let dW1 = matmul(matmul(nn.W2.transpose(), (o - y)) * dReLU(matmul(nn.W1, x) + nn.B1), x.transpose())
   let dB2 = o - y
-  let dB1 = matmul(nn.W2.transpose(), (o - y))
+  let dB1 = matmul(nn.W2.transpose(), (o - y)) * dReLU(matmul(nn.W1, x) + nn.B1)
 
   nn.W2 = nn.W2 - alpha * dW2
   nn.W1 = nn.W1 - alpha * dW1
@@ -98,10 +124,18 @@ proc backward (nn: var NN, o: seq[seq[float64]], y: seq[seq[float64]], x: seq[se
 
 proc init (nn: var NN): void =
   randomize()
-  nn.W1 = repeat(repeat(float64(0.0), 784), 24).map(proc (x: seq[float64]): seq[float64] {.closure.} = map(x, proc (y: float64): float64 = rand(2.0) - 1.0))
-  nn.W2 = repeat(repeat(float64(0.0), 24), 10).map(proc (x: seq[float64]): seq[float64] {.closure.} = map(x, proc (y: float64): float64 = rand(2.0) - 1.0))
-  nn.B1 = repeat(@[float64(0.0)], 24).map(proc (x: seq[float64]): seq[float64] = @[float64(rand(2.0) - 1.0)])
-  nn.B2 = repeat(@[float64(0.0)], 10).map(proc (x: seq[float64]): seq[float64] = @[float64(rand(2.0) - 1.0)])
+
+  proc initializeRandomWeights (row, column: int): seq[seq[float64]] =
+    result = newSeqWith(row, newSeq[float64](column))
+
+    for i in 0..<row:
+      for j in 0..<column:
+        result[i][j] = rand(2.0) - 1
+
+  nn.W1 = initializeRandomWeights(128, 784)
+  nn.W2 = initializeRandomWeights(10, 128)
+  nn.B1 = initializeRandomWeights(128, 1)
+  nn.B2 = initializeRandomWeights(10, 1)
 
 proc loadMNISTData (path: string): seq[MNISTData] =
   let f = open(path)
@@ -118,23 +152,27 @@ proc loadMNISTData (path: string): seq[MNISTData] =
 
     result.add(MNISTData(label: label, image: data))
 
-let trainingData = loadMNISTData("mnist/mnist_train.csv")
-let testData = loadMNISTData("mnist/mnist_test.csv")
+var trainingData = loadMNISTData("mnist/mnist_train.csv")
+var testData = loadMNISTData("mnist/mnist_test.csv")
 
 var nn = NN()
 nn.init()
 
-for data in trainingData:
-  let output = nn.forward(data.image)
-  var expected: seq[seq[float64]] = repeat(@[0.0'f64], 10)
-  expected[data.label][0] = 1.0'f64
-  nn.backward(output, expected, data.image, 0.001'f64)
-  let loss = categoricalCrossEntropyLoss(output, data.label)
+for epoch in 1..20:
+  trainingData.shuffle()
+  var alpha = 0.01'f64
+  for data in trainingData:
+    let output = nn.forward(data.image)
+    var expected: seq[seq[float64]] = repeat(@[0.0'f64], 10)
+    expected[data.label][0] = 1.0'f64
+    nn.backward(output, expected, data.image, alpha)
+    let loss = categoricalCrossEntropyLoss(output, data.label)
 
-  if loss.isNaN():
-    echo output
-    echo expected
-    break
+    if loss.isNaN():
+      echo output
+      echo expected
+      break
+  alpha *= 0.99
 
 var count = 0
 for data in testData:
